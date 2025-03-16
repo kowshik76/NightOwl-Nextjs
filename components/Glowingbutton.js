e85950d5d2f31024b91c1b7208b6ca8eded4f912
kowshik76 <kowshiksangada@gmail.com>
2025-03-16 21:28:04 +0530

    Add GlowingButton component, update About section text, and created Contact page

---
+++ components/Glowingbutton.js
@@ -0,0 +1,9 @@
+export default function GlowingButton() {
+    return (
+      <button className="relative px-6 py-3 font-semibold text-white bg-gray-900 rounded-lg border-2 border-transparent transition-all duration-300 ease-in-out 
+      hover:border-blue-500 hover:shadow-[0_0_15px_rgba(59,130,246,0.8)]">
+        Hover Me
+      </button>
+    );
+  }
+  
