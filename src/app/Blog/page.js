7e0387d98c62c888a410b85f087bd17424cc7f2a
kowshik76 <kowshiksangada@gmail.com>
2025-03-28 18:46:29 +0530

    Update blog data structure and implement dynamic blog retrieval API

--- src/app/Blog/page.js
+++ src/app/Blog/page.js
@@ -1,28 +1,47 @@
-import React from 'react'
-import Link from 'next/link'
-const page = () => {
+"use client";
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
+
+const Page = () => {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    console.log("I m running");
+    fetch("http://localhost:3000/api/blogs")
+      .then((a) => a.json())
+      .then((parsed) => {
+        console.log(parsed);
+        setBlogs(parsed);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []); // Add dependency array to ensure it runs only once
   return (
-    <div><div>
-    <div className="h-40vh w-100vh mt-8">
-      <div className="flex flex-col justify-center items-center">
-        {/* Blog 1 */}
-        <Link href="/blogspot/blog1">
-          <div className="h-40 w-100 m-5 rounded-2xl border-2 border-white transition-all duration-500
-          hover:border-blue-500 hover:shadow-[0_0_15px_rgba(59,130,246,0.8)] cursor-pointer">
-            <h4 className="text-center m-5 font-bold">This is the blog 1</h4>
-            <p className="text-center m-5">This is the paragraph of the first blog and the matter.</p>
+    <div>
+      <div>
+        <div className="h-40vh w-100vh mt-8">
+          <div className="flex flex-col justify-center items-center">
+            {/* Blog 1 */}
+            {blogs.map((blogitem) => {
+              return (
+                <Link href={`/blogspot/${blogitem.slug}`}>
+                  <div key={blogitem.slug} className="h-40 w-100 m-5 rounded-2xl border-2 border-white transition-all duration-500 hover:border-blue-500 hover:shadow-[0_0_15px_rgba(59,130,246,0.8)] cursor-pointer">
+                    <h4 className="text-center m-5 font-bold">
+                      {blogitem.Name}
+                    </h4>
+                    <p className="text-center m-5">
+                     {blogitem.Description.substr(0,100)}
+                    </p>
+                  </div>
+                </Link>
+              );
+            })}
           </div>
-        </Link>
-        {/* Blog 2 */}
-        <div className="h-40 w-100 rounded-2xl border-2 border-white transition-all duration-500
-        hover:border-blue-500 hover:shadow-[0_0_15px_rgba(59,130,246,0.8)] cursor-pointer">
-          <h4 className="text-center m-5 font-bold">This is the blog 2</h4>
-          <p className="text-center m-5">This is the paragraph of the second blog and the matter.</p>
         </div>
       </div>
     </div>
-  </div></div>
-  )
-}
+  );
+};
 
-export default page
+export default Page;
