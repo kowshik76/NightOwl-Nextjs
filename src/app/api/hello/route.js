bb8ba3c233b95f15dcea1192f2b01389dede3202
kowshik76 <kowshiksangada@gmail.com>
2025-03-28 13:40:49 +0530

    Remove old greeting API and add dynamic blog data retrieval API

--- src/app/api/hello/route.js
+++
@@ -1,5 +0,0 @@
-export async function GET(request) {
-    return new Response(JSON.stringify({ message: "Hello from the API!" }), {
-      headers: { "Content-Type": "application/json" },
-    });
-  }
